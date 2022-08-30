de508cb2f9549cc7eaf422349b29d37024c34654
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-08-30 16:41:12 +0300

    More robust script

--- avgresponse.sh
+++ avgresponse.sh
@@ -3,8 +3,10 @@
 #compute average time of responses of a proxy log for desired api path
 #an example: ./avgresponse.sh digitransit-proxy-6d856c4548-d8f8m  geocoding/v1/search
 
-kubectl logs $1 | grep $2 | grep -oE '[^ ]+$' > log.txt
-SUM=$(paste -sd+ log.txt | bc)
-COUNT=$(cat log.txt | wc -l)
-rm log.txt
+kubectl logs $1 > log.txt
+tail -500 log.txt > logtail.txt
+cat logtail.txt | grep $2 | grep -oE '[^ ]+$' > log2.txt
+SUM=$(paste -sd+ log2.txt | bc)
+COUNT=$(cat log2.txt | wc -l)
+rm log.txt logtail.txt log2.txt
 echo "$SUM/$COUNT" | bc -l
